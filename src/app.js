17d10bb45eac57baebb6904f868869aec07dad10
arit98 <aritradaschowdhury@gmail.com>
2025-12-25 16:34:05 +0530

    feat: implement core Express application with API routes and static file serving, and update Vercel config to serve public assets.

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import authRoute from "./routes/auth.route.js";
 import reviewRoute from "./routes/review.route.js";
 import bookingRoute from "./routes/booking.route.js";
 import fansRoute from "./routes/fans.route.js";
+import path from "path";
 
 const app = express();
 
@@ -22,7 +23,7 @@ app.use(cors({
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
-app.use(express.static("public"));
+app.use(express.static(path.join(process.cwd(), "public")));
 
 /* --- Routes --- */
 app.use("/api/v1/tours", tourRoute);
@@ -31,5 +32,9 @@ app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/review", reviewRoute);
 app.use("/api/v1/booking", bookingRoute);
 app.use("/api/v1/get-all-fans", fansRoute);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "public", "index.html"));
+});
 
 export default app;
